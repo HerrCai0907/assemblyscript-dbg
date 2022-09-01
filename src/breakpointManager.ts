a931dbdac3fe231bd0f7091fc595f11aa3191f94
Congcong Cai <congcongcai0907@163.com>
2022-09-01 12:58:17 +0800

    fix: add breakpoint when mutliple instruction in one line

--- src/breakpointManager.ts
+++ src/breakpointManager.ts
@@ -1,3 +1,4 @@
+import { BREAKPOINT_DEBUG } from "./constant";
 import { WasmDebuggerClient } from "./proto/interface_grpc_pb";
 import * as proto from "./proto/interface_pb";
 import { source2instr } from "./sourceMap";
@@ -5,76 +6,93 @@ import { source2instr } from "./sourceMap";
 type FilePath = string;
 type Line = number;
 type BreakpointIndex = number;
-
-const INVALID_BREAKPOINT_INDEX = -1;
+type FuncBreakpointIndex = () => Promise<BreakpointIndex | undefined>;
+type LineMap = Map<Line, (BreakpointIndex | FuncBreakpointIndex)[]>;
 
 export class BreakpointManager {
-  breakpoints: Map<FilePath, Map<Line, BreakpointIndex>> = new Map();
+  breakpoints: Map<FilePath, LineMap> = new Map();
   constructor(
     private _client: WasmDebuggerClient,
-    private _sourceToInstrMapping: Map<FilePath, Map<Line, { funcIndex: number; instrIndex: number }>>,
+    private _sourceToInstrMapping: Map<FilePath, Map<Line, { funcIndex: number; instrIndex: number }[]>>,
     private _errorHandler: (reason: string) => void
   ) {}
 
   async syncBreakpoints() {
-    for (const [path, lineMap] of this.breakpoints) {
-      for (const [line, bpIndex] of lineMap) {
-        if (bpIndex == INVALID_BREAKPOINT_INDEX) {
-          const codePosition = source2instr({ source: path, line }, this._sourceToInstrMapping);
-          if (codePosition == null) {
-            lineMap.delete(line);
-            continue;
-          }
-          const breakpointIndex = await this.addBreakpoint(codePosition.funcIndex, codePosition.instrIndex);
-          if (breakpointIndex != undefined) {
-            lineMap.set(line, breakpointIndex);
+    for (const [_, lineMap] of this.breakpoints) {
+      for (const [line, bpIndexs] of lineMap) {
+        const newBreakpointIndexs: BreakpointIndex[] = [];
+        for (let i = 0; i < bpIndexs.length; i++) {
+          const bpIndex = bpIndexs[i];
+          if (typeof bpIndex == "function") {
+            const updatedIndex = await bpIndex();
+            if (updatedIndex != undefined) {
+              newBreakpointIndexs.push(updatedIndex);
+            }
           } else {
-            lineMap.delete(line);
+            newBreakpointIndexs.push(bpIndex);
           }
         }
+        lineMap.set(line, newBreakpointIndexs);
       }
     }
   }
 
-  async updataBreakpoints(path: FilePath, lines: Line[], updateImmediate: boolean): Promise<Map<Line, BreakpointIndex>> {
+  async updataBreakpoints(path: FilePath, lines: Line[], updateImmediate: boolean): Promise<LineMap> {
     const oldLines = this.breakpoints.get(path);
-    const newLines = new Map<Line, BreakpointIndex>();
-    if (oldLines == undefined) {
-      for (let line of lines) {
-        const codePosition = source2instr({ source: path, line }, this._sourceToInstrMapping);
-        if (codePosition == null) {
-          continue;
-        }
-        const breakpointIndex = updateImmediate
-          ? await this.addBreakpoint(codePosition.funcIndex, codePosition.instrIndex)
-          : INVALID_BREAKPOINT_INDEX;
-        if (breakpointIndex != undefined) {
-          newLines.set(line, breakpointIndex);
-        }
-      }
-    } else {
-      const linesSet = new Set(lines);
+    const newLines: LineMap = new Map();
+    const linesSet = new Set(lines);
+    if (oldLines) {
       // update old breakpoints
-      for (const [line, breakpointIndex] of oldLines) {
+      for (const [line, breakpointIndexs] of oldLines) {
         if (linesSet.has(line)) {
-          newLines.set(line, breakpointIndex);
+          // both in new and old
+          newLines.set(line, breakpointIndexs);
           linesSet.delete(line);
         } else {
-          await this.removeBreakpoint(breakpointIndex);
+          // only in old
+          await Promise.all(
+            breakpointIndexs.map(async (breakpointIndex, index) => {
+              if (typeof breakpointIndex == "number") {
+                if (updateImmediate) {
+                  await this.removeBreakpoint(breakpointIndex);
+                } else {
+                  if (BREAKPOINT_DEBUG) console.log(`cache remove bp ${breakpointIndex}`);
+                  breakpointIndexs[index] = async () => {
+                    this.removeBreakpoint(breakpointIndex);
+                    return undefined;
+                  };
+                }
+              }
+            })
+          );
         }
       }
-      // update new breakpoints
-      for (let line of linesSet) {
-        const codePosition = source2instr({ source: path, line: line }, this._sourceToInstrMapping);
-        if (codePosition == null) {
-          continue;
+    }
+    // update new breakpoints
+    for (let line of linesSet) {
+      const codePositions = source2instr({ source: path, line }, this._sourceToInstrMapping);
+      if (codePositions.length == 0) {
+        continue;
+      }
+      if (updateImmediate) {
+        // communicate with server immediately
+        const breakpointIndexs: BreakpointIndex[] = [];
+        for (const codePosition of codePositions) {
+          const breakpointIndex = await this.addBreakpoint(codePosition.funcIndex, codePosition.instrIndex);
+          if (breakpointIndex != undefined) {
+            breakpointIndexs.push(breakpointIndex);
+          }
         }
-        const breakpointIndex = updateImmediate
-          ? await this.addBreakpoint(codePosition.funcIndex, codePosition.instrIndex)
-          : INVALID_BREAKPOINT_INDEX;
-        if (breakpointIndex != undefined) {
-          newLines.set(line, breakpointIndex);
+        if (breakpointIndexs.length != 0) {
+          newLines.set(line, breakpointIndexs);
         }
+      } else {
+        // store the notify call
+        if (BREAKPOINT_DEBUG) console.log(`cache set bp ${JSON.stringify(codePositions)}`);
+        newLines.set(
+          line,
+          codePositions.map((codePosition) => () => this.addBreakpoint(codePosition.funcIndex, codePosition.instrIndex))
+        );
       }
     }
     this.breakpoints.set(path, newLines);
@@ -98,6 +116,7 @@ export class BreakpointManager {
       return;
     }
     const breakpointIndex = reply.getBreakpointIndex();
+    if (BREAKPOINT_DEBUG) console.log(`set bp (${funcIndex},${instrIndex}), index ${breakpointIndex}`);
     return breakpointIndex;
   }
 
@@ -115,6 +134,8 @@ export class BreakpointManager {
     });
     if (reply.getStatus() == proto.Status.NOK) {
       this._errorHandler(`set breakpoint failed due to "${reply.getErrorReason()}"`);
+      return;
     }
+    if (BREAKPOINT_DEBUG) console.log(`remove bp index ${breakpointIndex}`);
   }
 }
